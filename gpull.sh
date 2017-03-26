d83889ec44bdb23f9f9c136fd106113a3eca662d
Hrishi Dixit <uqbar@users.noreply.github.com>
2017-03-26 00:06:58 -0400

    Add a `git fetch` before pulling `master`

--- gpull.sh
+++ gpull.sh
@@ -16,6 +16,7 @@ find * -prune -type d | while read d; do
   if [ -d ".git" ]; then
     echo ""
     echo "----> Pulling ${CYAN}$d${NC}"
+    git fetch
     git checkout master
     git pull origin master
   else
